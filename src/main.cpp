7cfd68afb395e71afb867e14811d57558ea46835
BrunoWilkinson <bruno.wilkinson@gmail.com>
2022-10-05 15:07:09 +0100

    chore: file setup

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "numword.h"
+
+int main()
+{
+    core::numword nw;
+    uint64_t n;
+
+    n = 3; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 47; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 73; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 1492; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 10012; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 100073; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 1000000; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 1000000001; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 123000000000; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 474142398123; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 1474142398007; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 999000000000000; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 999999999999999999; printf("n is %lld, %s\n", n, nw.words(n));
+    n = 1000000000000000000; printf("n is %lld, %s\n", n, nw.words(n));
+
+    return 0;
+}
---
+++ src/numword.h
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <string>
+
+namespace core {
+    class numword {
+        public:
+            std::string words(uint64_t n);
+    };
+};
